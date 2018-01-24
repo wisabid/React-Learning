2de3987b10a0d4c76c69934aa8360bcf6133705e
Shamimpm <212340778@logon.ds.ge.com>
2018-01-24 20:03:58 +0530

    Committing react apps

---
+++ react-from-scratch/containers/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render () {
+        return (
+        	<div style={{display:'flex', justifyContent : 'center'}}>
+        		<h1>Alfie! This is my first react App without create-react-app</h1>
+        	</div>
+        )
+    }
+}
---
+++ react-from-scratch/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+//Ref : https://codeburst.io/setting-up-a-react-project-from-scratch-d62f38ab6d97
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
